d0f1381e50d8ab570c78adbedbbfc5561dcae222
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-01-25 18:20:53 +0900

    feat: fetch notion api noricare FAQ

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,10 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {Client} from "@notionhq/client";
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-type Data = {
-  name: string;
-};
+const notionSecret =  process.env.NOTION_SECRET
+const FAQ_NORICARE_DB_ID=process.env.NOTION_DB_FAQ_NORICARE_ID
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'John Doe' });
+
+const notion = new Client({auth:notionSecret})
+
+export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
+ if(
+  !notionSecret || !FAQ_NORICARE_DB_ID
+ )throw new Error("Missing notion secret or DB-ID");
+
+ const query = await notion.databases.query({
+  database_id:FAQ_NORICARE_DB_ID
+ })
+
+ console.log(query)
+
+ res.status(200).json({name:"ayeon"})
 }
