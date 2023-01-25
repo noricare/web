6cca7e9fba8e5801ab5c04215722577f5fa2e4da
JangAyeon <67853616+JangAyeon@users.noreply.github.com>
2023-01-25 19:01:09 +0900

    chore: next config js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    NOTION_DB_FAQ_NORICARE_ID: process.env.NOTION_DB_FAQ_NORICARE_ID,
+    NOTION_SECRET:process.env.NOTION_SECRET
+},
 };
 
 module.exports = nextConfig;
